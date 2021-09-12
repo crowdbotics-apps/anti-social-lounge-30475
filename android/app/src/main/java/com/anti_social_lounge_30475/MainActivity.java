a26b3851b8be1f0dfd000a69984607f316abb50b
Crowdbotics <team@crowdbotics.com>
2021-09-12 03:33:22 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/anti_social_lounge_30475/MainActivity.java
@@ -0,0 +1,15 @@
+package com.anti_social_lounge_30475;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "anti_social_lounge_30475";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "anti_social_lounge_30475Identifier",
+  url: "https://anti_social_lounge_30475Identifier.botics.co",
+  api: "https://anti_social_lounge_30475Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
